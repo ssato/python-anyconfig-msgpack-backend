8af7851d96f3fc602a37f374118fa31fc7ca744b
Satoru Sato <satoru.satoh@gmail.com>
2025-08-17 14:43:01 +0900

    change: add and switch to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
-"""setup.py to build packages.
-"""
-from __future__ import absolute_import
-
-import glob
+"""setup.py to build package."""
 import os
+import pathlib
 import re
 import setuptools
 import setuptools.command.bdist_rpm
 
 
-VERSION = False
-for pyf in glob.glob("src/*/__init__.py"):
-    matches = [m.groups() for m in (re.match(r'__version__ = "([0-9.]+)"', l)
-                                    for l in open(pyf)) if m]
-    if matches:
-        VERSION = matches[0][0]
-        break
+# It might throw IndexError and so on.
+VERSION = '0.1.0'
+VER_REG = re.compile(r"^__version__ = '([^']+)'")
 
-assert VERSION
+for fpath in pathlib.Path('src').glob('**/__init__.py'):
+    for line in fpath.open():
+        match = VER_REG.match(line)
+        if match:
+            VERSION = match.groups()[0]
+            break
 
 # For daily snapshot versioning mode:
 RELEASE = "1%{?dist}"
 if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
     import datetime
-    RELEASE = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    RELEASE = RELEASE.replace('1',
+                              datetime.datetime.now().strftime("%Y%m%d"))
 
 
 def _replace(line):
-    """Replace some strings in the RPM SPEC template"""
+    """Replace some strings in the RPM SPEC template."""
     if "@VERSION@" in line:
         return line.replace("@VERSION@", VERSION)
 
@@ -40,19 +39,17 @@ def _replace(line):
     return line
 
 
-# pylint: disable=invalid-name
 class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
-    """Override the default content of the RPM SPEC.
-    """
-    spec_tmpl = os.path.join(os.path.abspath(os.curdir),
-                             "pkg/package.spec.in")
+    """Override the default content of the RPM SPEC."""
+
+    spec_tmpl = pathlib.Path('pkg/package.spec.in').resolve()
 
     def _make_spec_file(self):
-        return [_replace(l.rstrip()) for l in open(self.spec_tmpl)]
+        """Generate the RPM SPEC file."""
+        return [_replace(line.rstrip()) for line in self.spec_tmpl.open()]
 
 
-setuptools.setup(version=VERSION,
-                 package_dir={'': 'src'},
-                 cmdclass=dict(bdist_rpm=bdist_rpm))
-
-# vim:sw=4:ts=4:et:
+setuptools.setup(
+    version=VERSION,
+    cmdclass=dict(bdist_rpm=bdist_rpm),
+)
